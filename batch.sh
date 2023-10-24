6bd62838dee73c57b638cf86d85a0ce531c621f7
boticus999 <a.zherdev93@gmail.com>
2023-10-24 12:59:15 +0300

    initial commit

--- batch.sh
+++ batch.sh
@@ -1,8 +1,8 @@
 git init
-git checkout -b master
+git checkout -b master #автоматически не создалась 
 git add .
 git commit -m "initial commit"
-git checkout -b sidebr
+git checkout -b sidebranch
 mkdir blog && cd "$_"
 touch index.js index.html
 git remote add origin https://github.com/boticus999/origin.git
